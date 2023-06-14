9a91024bc0eb30e4b3583aa2fd0b2e617ae2cb38
RajaAzhary <nr.saputra14@gmail.com>
2023-06-14 17:35:12 +0700

    home

--- app/src/main/java/org/d3if3151/pt2/HomeActivity.java
+++ app/src/main/java/org/d3if3151/pt2/HomeActivity.java
@@ -1,8 +1,14 @@
 package org.d3if3151.pt2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +16,20 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        SharedPreferences sharedpreferences = getSharedPreferences("shared_prefs", Context.MODE_PRIVATE);
+        String username = sharedpreferences.getString("Username", "").toString();
+        Toast.makeText(getApplicationContext(), "Welcome", Toast.LENGTH_SHORT).show();
+
+        CardView exit = findViewById(R.id.cardExit);
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor = sharedpreferences.edit();
+                editor.clear();
+                editor.apply();
+                startActivity(new Intent(HomeActivity.this,LoginActivity.class));
+            }
+        });
     }
 }

6cf8eac9116444f0567548084925a36cadc11c78
RajaAzhary <nr.saputra14@gmail.com>
2023-06-14 23:24:42 +0700

    doctor details

--- app/src/main/java/org/d3if3151/pt2/FindDoctorActivity.java
+++ app/src/main/java/org/d3if3151/pt2/FindDoctorActivity.java
@@ -1,8 +1,11 @@
 package org.d3if3151.pt2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class FindDoctorActivity extends AppCompatActivity {
 
@@ -10,5 +13,63 @@ public class FindDoctorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_doctor);
+
+        CardView exit = findViewById(R.id.cardFDBack);
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(FindDoctorActivity.this, HomeActivity.class));
+            }
+        });
+        CardView familyphycisian = findViewById(R.id.cardFDFamilyPhysician);
+        familyphycisian.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(FindDoctorActivity.this, DoctorDetailsActivity.class);
+                it.putExtra("title", "Mental Health Counsellor");
+                startActivity(it);
+            }
+        });
+
+        CardView dietician = findViewById(R.id.cardFDDietician);
+        familyphycisian.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(FindDoctorActivity.this, DoctorDetailsActivity.class);
+                it.putExtra("title", "Counselling Psychology");
+                startActivity(it);
+            }
+        });
+
+        CardView dentist = findViewById(R.id.cardFDDentist);
+        familyphycisian.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(FindDoctorActivity.this, DoctorDetailsActivity.class);
+                it.putExtra("title", "Psychotherapy");
+                startActivity(it);
+            }
+        });
+
+        CardView surgeon = findViewById(R.id.cardFDSurgeon);
+        familyphycisian.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(FindDoctorActivity.this, DoctorDetailsActivity.class);
+                it.putExtra("title", "Clinical Theraphy");
+                startActivity(it);
+            }
+        });
+
+        CardView cardiologist = findViewById(R.id.cardFDCardiologist);
+        familyphycisian.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(FindDoctorActivity.this, DoctorDetailsActivity.class);
+                it.putExtra("title", "Rehabilitation");
+                startActivity(it);
+            }
+        });
+
     }
 }
